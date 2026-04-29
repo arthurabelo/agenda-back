28d27dfec72b704f0265cafbbf52168655557869
Arthur Rabelo <arthur.rabelo@tjpi.jus.br>
2026-04-29 13:24:17 -0300

    Adicionar CrossOrigin

--- src/main/java/br/jus/tjpi/agendatelefonica/controller/UsuarioController.java
+++ src/main/java/br/jus/tjpi/agendatelefonica/controller/UsuarioController.java
@@ -16,6 +16,7 @@ import br.jus.tjpi.agendatelefonica.service.LoginService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
