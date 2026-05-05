86b7ac4a6e98dbb2e530e47569d31c40a2c0a4c4
Arthur Rabelo <arthur.rabelo@tjpi.jus.br>
2026-05-05 15:01:28 -0300

    Remover CrossOrigin

--- src/main/java/br/jus/tjpi/agendatelefonica/controller/ContatoController.java
+++ src/main/java/br/jus/tjpi/agendatelefonica/controller/ContatoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -84,7 +83,7 @@ public class ContatoController {
             @RequestParam(required = false) String termo) {
         
         // Se ambos forem nulos ou vazios, retorna todos
-        if ((termo == null || termo.isBlank()) && (termo == null || termo.isBlank())) {
+        if (termo == null || termo.isBlank()) {
             return ResponseEntity.ok(repository.findAll());
         }
 
