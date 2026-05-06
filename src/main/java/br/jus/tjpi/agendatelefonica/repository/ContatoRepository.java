278026448e643b14b06984688eac09d56ae4279e
Kleberson Silva Vilela <kleberson.vilela@tjpi.jus.br>
2026-05-06 15:53:46 +0000

    Update ContatoRepository.java

--- src/main/java/br/jus/tjpi/agendatelefonica/repository/ContatoRepository.java
+++ src/main/java/br/jus/tjpi/agendatelefonica/repository/ContatoRepository.java
@@ -6,17 +6,29 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
-public interface ContatoRepository extends JpaRepository<Contato, Long> {
+public interface ContatoRepository  extends JpaRepository<Contato, Long> {
     
+    List<Contato> findByUnidadeContainingIgnoreCase(String unidade);
+
     List<Contato> findBySetorContainingIgnoreCase(String setor);
+
+    List<Contato> findByComarcaContainingIgnoreCase(String comarca);
+    
+    List<Contato> findByLocalidadeContainingIgnoreCase(String localidade);
+
+    List<Contato> findByTelefoneContainingIgnoreCase(String telefone);
     
     @Query("SELECT c FROM Contato c WHERE " +
+           "(:termo IS NULL OR (" +
            "LOWER(c.unidade) LIKE LOWER(CONCAT('%', :termo, '%')) OR " +
            "LOWER(c.setor) LIKE LOWER(CONCAT('%', :termo, '%')) OR " +
            "LOWER(c.localidade) LIKE LOWER(CONCAT('%', :termo, '%')) OR " +
-           "LOWER(c.comarca) LIKE LOWER(CONCAT('%', :termo, '%')) OR " +
+           "LOWER(c.comarca) LIKE LOWER(CONCAT('%', :termo, '%')) OR " + // Adicionado comarca aqui também
            "LOWER(c.endereco) LIKE LOWER(CONCAT('%', :termo, '%')) OR " +
            "LOWER(c.telefone) LIKE LOWER(CONCAT('%', :termo, '%')) OR " +
-           "LOWER(c.tipoContato) LIKE LOWER(CONCAT('%', :termo, '%'))")
-    List<Contato> findByFiltroGlobal(@Param("termo") String termo);
+           "LOWER(c.tipoContato) LIKE LOWER(CONCAT('%', :termo, '%'))" + // Novo campo tipo_contato
+           "))")
+    List<Contato> findByFiltroGlobal(
+        @Param("termo") String termo
+    );
 }
