1619b6c3c72bee6c1f01d1d324810f128becf797
Arthur Rabelo <arthur.rabelo@tjpi.jus.br>
2026-05-18 10:59:36 -0300

    Security: Impedir alterações por usuários

--- src/main/java/br/jus/tjpi/agendatelefonica/config/SecurityConfig.java
+++ src/main/java/br/jus/tjpi/agendatelefonica/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.jus.tjpi.agendatelefonica.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +15,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
