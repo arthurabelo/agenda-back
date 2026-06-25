a8febe911086fada4c8a65233956670668d764d1
Kleberson Silva Vilela <kleberson.vilela@tjpi.jus.br>
2026-06-25 15:53:19 +0000

    Update ContatoRepository.java - removendo duplicação do metodo findDistinctComarcas()

--- src/main/java/br/jus/tjpi/agendatelefonica/repository/ContatoRepository.java
+++ src/main/java/br/jus/tjpi/agendatelefonica/repository/ContatoRepository.java
@@ -34,9 +34,6 @@ public interface ContatoRepository  extends JpaRepository<Contato, Long> {
     )
     Page<Contato> findByFiltroGlobal(@Param("termo") String termo, Pageable pageable);
 
-        // Query para trazer a lista limpa de comarcas pro select do front
-    @Query("SELECT DISTINCT LOWER(c.comarca) FROM Contato c WHERE c.comarca IS NOT NULL ORDER BY c.comarca ASC")
-    List<String> findDistinctComarcas();
 
     // Query do Relatório com filtros condicionais (Se o parâmetro for nulo, o Spring ignora ele)
    @Query("SELECT DISTINCT LOWER(c.comarca) FROM Contato c WHERE c.comarca IS NOT NULL ORDER BY c.comarca ASC")
